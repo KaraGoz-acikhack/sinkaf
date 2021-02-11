fd8d6da29a884214058ba055b0a9253b1d8aaddc
eonurk <onurkara3594@gmail.com>
2021-02-11 13:07:58 -0500

    version bump
    - model selection
    - better user experience lol
    - cildirmaca

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sinkaf", # Replace with your own username
-    version="0.0.4",
+    version="0.0.5",
     author="E Onur Karakaslar",
     author_email="eonurkara@gmail.com",
     description="Türkçe küfürlü içerikleri bulan bir kütüphane",
